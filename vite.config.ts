900d3079d4bad76b0ec8e0cda02421d33773d971
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2025-06-09 07:21:42 +0530

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [
     vue({
       template: { transformAssetUrls }
     }),
-    quasar()
+    quasar(),
+    VueDevTools({
+      enabled: true,
+      appendTo: 'body'
+    })
   ],
   define: {
     __VUE_PROD_DEVTOOLS__: true
+  },
+  server: {
+    port: 3000,
+    open: true
   }
 })
